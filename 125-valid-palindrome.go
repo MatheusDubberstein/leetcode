f8697764fa3e540a41075bd46a73d66fe12e214b
MatheusDubberstein <matheusdub.s@gmail.com>
2025-03-18 11:00:48 -0300

    125-valid-palindrome

---
+++ 125-valid-palindrome.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func reverseString(s string) string {
+	var result string
+	for _, x := range s {
+		result = string(x) + result
+	}
+	return result
+}
+
+func isPalindrome(s string) bool {
+	alphaNumericRegex := regexp.MustCompile("[^a-zA-Z0-9]+")
+
+	s = strings.ToLower(alphaNumericRegex.ReplaceAllString(s, ""))
+	reverseS := reverseString(s)
+	return reverseS == s
+}
+
+func main() {
+	var result bool
+	result = isPalindrome("A man, a plan, a canal: Panama")
+	fmt.Println(result)
+
+	result = isPalindrome("race a car")
+	fmt.Println(result)
+
+	result = isPalindrome(" ")
+	fmt.Println(result)
+}
